cb4582a327aef15e637c92b2ad13d915ba194890
Sean Fisher <srtfisher@gmail.com>
2013-01-28 13:55:20 -0500

    Moving OAuth class under namespacing

--- lib/Drupal/wconsumer/Rest/Request.php
+++ lib/Drupal/wconsumer/Rest/Request.php
@@ -1,7 +1,17 @@
 <?php
 namespace Drupal\wconsumer\Rest;
 
+/**
+ * REST Request Class
+ *
+ * @package wconsumer
+ * @subpackage request
+ */
 class Request
 {
-
+  /**
+   * API Base URL
+   * @var string
+   */
+  public $apiURL;
 }
