3606f2cdc41084a6ba0dcd14b988d36b31995e21
Sean Fisher <srtfisher@gmail.com>
2013-01-31 14:03:42 -0500

    Service Registry and starting to work on forms

--- lib/Drupal/wconsumer/Service.php
+++ lib/Drupal/wconsumer/Service.php
@@ -3,6 +3,14 @@ namespace Drupal\wconsumer;
 
 class Service {
   /**
+   * Internal Service Registry
+   *
+   * @var array
+   * @access private
+   */
+  private static $_services = NULL;
+
+  /**
    * Return Active Services
    *
    * Static Method
@@ -13,6 +21,26 @@ class Service {
    */
   public static function services($services = array())
   {
-    return module_invoke_all('wconsumer_config', $services);
+    if (self::$_services == NULL)
+      self::$_services = module_invoke_all('wconsumer_config', $services);
+
+    return self::$_services;
+  }
+
+  /**
+   * Retrieve the Object for a Service
+   *
+   * @param string Service
+   * @return object
+   * @throws Drupal\wconsumer\Exception
+   */
+  public static function getObject($service, $services = array())
+  {
+    $services = self::services($services);
+
+    if (!isset($services[$service]))
+      throw new Exception('Unknown service: '.$service);
+
+    return $services[$service];
   }
 }
