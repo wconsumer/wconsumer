44934de7de32c11f2a10938bfea1605a8b62d933
Sean Fisher <srtfisher@gmail.com>
2013-01-30 19:23:43 -0500

    Interface methods

--- lib/Drupal/wconsumer/Common/RequestInterface.php
+++ lib/Drupal/wconsumer/Common/RequestInterface.php
@@ -3,4 +3,37 @@ namespace Drupal\wconsumer\Common;
 
 interface RequestInterface {
 
+  /**
+   * Set the API URL
+   *
+   * @param string
+   */
+  public function setApiUrl($url);
+
+  /**
+   * Retrieve the API URL
+   *
+   * @return string
+   */
+  public function getApiUrl();
+
+  /**
+   * Make a Request Manually
+   *
+   * @param  string
+   * @param  string HTTP method (POST, GET, etc)
+   * @param  array
+   * @return  mixed
+   */
+  public function makeRequest($endPoint, $method, $arguments);
+
+  /**
+   * Magic Method to Make Requests
+   *
+   * ```
+   * $object->get('end/point.json', array(...));
+   * ```
+   * @return mixed
+   */
+  public function __call($method, $arguments);
 }
