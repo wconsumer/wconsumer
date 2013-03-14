00b3fb4a6f385350c883836029af39139c9a644e
Sean Fisher <srtfisher@gmail.com>
2013-03-14 11:24:30 -0400

    API Documentation

--- wconsumer.api.php
+++ wconsumer.api.php
@@ -1,7 +1,53 @@
 <?php
-  function wconsumer_init() {
+/**
+ * @file
+ * Hooks provided by wconsumer Core
+ */
+
+/**
+ * LinkedIn Service Configuration
+ *
+ * @return array
+ */
+function hook_wconsumer_config($services) {
+  // Instantiate a new service object for this service module
+  $service_object = new service_object();
+
+  // Setup Request Interface
+  // For this module, we're going to be interfacing with a REST based API
+  // To implement a custom Requests interface, look at 
+  // `lib/Drupal/wconsumer/Rest/Request.php`
+  $service_object->request = new Drupal\wconsumer\Rest\Request();
+
+  // Define the base of the REST API URL
+  $service_object->request->apiURL = 'http://api.linkedin.com/v1/';
+
+  // Authentication for the Service
+  // This scenario would be OAuth1
+  $service_object->request->authencation = new Drupal\wconsumer\Rest\Authentication\Oauth($service_object);
+
+  // For the authentication method, you would have to provide settings such as these for OAuth 1
+  $service_object->request->authencation->accessTokenURL = 'https://api.linkedin.com/uas/oauth/accessToken';
+  $service_object->request->authencation->authenticateURL = 'https://api.linkedin.com/uas/oauth/authenticate';
+  $service_object->request->authencation->authorizeURL = 'https://www.linkedin.com/uas/oauth/authenticate';
+  $service_object->request->authencation->requestTokenURL = 'https://api.linkedin.com/uas/oauth/requestToken';
+
+  // Service Specific Options
+  // These settings are on a per-service basis such as allow the user to log in and create
+  // and account with this service.
+  $service_object->options = new Drupal\wconsumer\Common\Options;
+  $service_object->options->enable('allow login');
   
-    $service = module_invoke_all('wc_config', $service);
-    print_r($service);
-    drupal_set_message('<pre>' . print_r($service, TRUE) . '</pre>';
+  // Register the service into our configuration array
+  // The key of the array below, "linkedin", is the internal name used to identify this service
+  $services['linkedin'] = $service_object;
+
+  return $services;
+}
+
+/**
+ * The Dummy Service Object
+ */
+class service_object extends Drupal\wconsumer\ServiceBase {
+
 }
