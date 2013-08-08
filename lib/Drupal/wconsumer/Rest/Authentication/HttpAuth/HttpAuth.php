56b04a0a6a8765e5a1ec40fbff50e745b77819a5
dangoodman <dan.ponomarev@gmail.com>
2013-08-09 00:03:57 +0400

    HttpAuth fixed for using new Credentials

--- lib/Drupal/wconsumer/Rest/Authentication/HttpAuth/HttpAuth.php
+++ lib/Drupal/wconsumer/Rest/Authentication/HttpAuth/HttpAuth.php
@@ -10,152 +10,52 @@ namespace Drupal\wconsumer\Rest\Authentication\HttpAuth;
 use Drupal\wconsumer\Rest\Authentication as AuthencationBase;
 use Drupal\wconsumer\Common\AuthInterface;
 use Guzzle\Plugin\CurlAuth\CurlAuthPlugin as GuzzleHttpAuth;
-use Drupal\wconsumer\Exception as WcException;
-use Drupal\wconsumer\ServiceBase;
 use Guzzle\Http\Client;
 
 
 /**
  * HTTP Authentication
  *
- * Used for services that require a specific HTTP username and/or password
+ * Used for services that require a specific HTTP username and password
  *
  * @package wconsumer
  * @subpackage request
  */
 class HttpAuth extends AuthencationBase implements AuthInterface {
-  /**
-   * Define if they need a username
-   *
-   * @var boolean
-   */
-  private $needsUsername;
-
-  /**
-   * Define if they need a password
-   *
-   * @var boolean
-   */
-  private $needsPassword;
-
-
-
-  public function __construct(ServiceBase $instance = null, $needsUsername = false, $needsPassword = false)
-  {
-    parent::__construct($instance);
-
-    $this->needsUsername = $needsUsername;
-    $this->needsPassword = $needsPassword;
-  }
-
-  /**
-   * Format Registry Credentials
-   *
-   * @param array $data
-   * @return array
-   *
-   * @throws WcException
-   */
-  public function formatServiceCredentials($data)
-  {
-    if ($this->needsUsername && empty($data['username']))
-      throw new WcException('HTTP Auth requires username and it is not set or is empty.');
-
-    if ($this->needsPassword && empty($data['password']))
-      throw new WcException('HTTP Auth requires password and it is not set or is empty.');
-
-    return array(
-      'username' => ($this->needsUsername) ? $data['username'] : null,
-      'password' => ($this->needsPassword) ? $data['password'] : null,
-    );
-  }
-
-  /**
-   * Format the Saved Credentials
-   *
-   * Not used in HTTP Auth API
-   *
-   * @param array
-   * @return array Empty array
-   */
-  public function formatCredentials($data)
-  {
-    return array();
-  }
-
-  /**
-   * Validate if they're setup
-   *
-   * @param string
-   * @return boolean
-   */
   public function is_initialized($type = 'user')
   {
     switch($type) {
       case 'system' :
-        $registry = $this->_instance->getServiceCredentials();
-        if (! $registry OR ! isset($registry->credentials)) return FALSE;
-
-        if ($this->needsUsername AND empty($registry->credentials['username']))
-          return FALSE;
-
-        if ($this->needsPassword AND empty($registry->credentials['password']))
-          return FALSE;
-
-        return TRUE;
-        break;
+        return ($this->_instance->getServiceCredentials() !== null);
+      break;
 
       case 'user' :
         return TRUE;
-        break;
+      break;
 
       default :
         return FALSE;
+      break;
     }
   }
 
-  /**
-   * Sign the request before sending it off
-   *
-   * @param Client $client
-   * @access private
-   */
   public function sign_request(&$client)
   {
-    $registry = $this->_instance->getServiceCredentials();
-
-    // Add the auth plugin to the client object
-    $authPlugin = new GuzzleHttpAuth(
-      ($this->needsUsername) ? $registry->credentials['username'] : '',
-      ($this->needsPassword) ? $registry->credentials['password'] : ''
-    );
-
-    $client->addSubscriber($authPlugin);
+    $credentials = $this->_instance->getServiceCredentials();
+    $client->addSubscriber(new GuzzleHttpAuth($credentials->token, $credentials->secret));
   }
 
   /**
-   * Authenticate the User
-   *
-   * Not needed for HTTP Auth
-   *
    * @codeCoverageIgnore
    */
   public function authenticate(&$user) { }
 
   /**
-   * Log the user out
-   *
-   * Not needed for HTTP Auth
-   *
    * @codeCoverageIgnore
    */
   public function logout(&$logout) { }
 
   /**
-   * Callback
-   *
-   * Not needed for HTTP Auth
-   *
    * @codeCoverageIgnore
    */
   public function onCallback(&$user, $values) { }
