160c0fc09076af9fa80ae1fe10362edb4e88668d
dangoodman <dan.ponomarev@gmail.com>
2013-08-09 00:01:25 +0400

    + Credentials class keeping token and secret (e.g. username and password)

---
+++ tests/Rest/Authentication/CredentialsTest.php
@@ -0,0 +1,42 @@
+<?php
+namespace Drupal\wconsumer\Tests\Rest\Authentication;
+
+use Drupal\wconsumer\Rest\Authentication\Credentials;
+
+
+
+class CredentialsTest extends \PHPUnit_Framework_TestCase {
+  public function testConstruction() {
+    $credentials = new Credentials(123, 'abc');
+    $this->assertSame('123', $credentials->token);
+    $this->assertSame('abc', $credentials->secret);
+  }
+
+  /**
+   * @expectedException \Drupal\wconsumer\Exception
+   */
+  public function testConstructionFailsOnEmptyToken() {
+    new Credentials(null, 'abc');
+  }
+
+  /**
+   * @expectedException \Drupal\wconsumer\Exception
+   */
+  public function testConstructionFailsOnEmptySecret() {
+    new Credentials('johndoe', '');
+  }
+
+  public function testFromArrayConstruction() {
+    $credentials = Credentials::fromArray(array(
+      'token' => 'token',
+      'secret' => 'secret',
+      'dummy' => 'skip'
+    ));
+
+    $this->assertSame('token', $credentials->token);
+    $this->assertSame('secret', $credentials->secret);
+
+    /** @noinspection PhpUndefinedFieldInspection */
+    $this->assertNull($credentials->dummy);
+  }
+}
