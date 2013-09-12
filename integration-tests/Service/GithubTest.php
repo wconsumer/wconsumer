ec0ae0611a6a40eff6b8b86f9e32dbd739df8595
dangoodman <dan.ponomarev@gmail.com>
2013-09-12 16:08:46 +0400

    Adding a test for Github->api()->post() and ->delete()

--- integration-tests/Service/GithubTest.php
+++ integration-tests/Service/GithubTest.php
@@ -11,11 +11,7 @@ class GithubTest extends DrupalTestBase {
   public function testApi() {
     $GLOBALS['user'] = (object)array('uid' => 56);
 
-    $github = new Github();
-
-    $github->setServiceCredentials(Credentials::fromArray($this->keys('github', 'app')));
-    $github->setCredentials(Credentials::fromArray($this->keys('github', 'user')));
-
+    $github = $this->github();
     $api = $github->api();
 
     $response = $api->get('/user/followers')->send();
@@ -23,5 +19,42 @@ class GithubTest extends DrupalTestBase {
     $this->assertTrue($response->isSuccessful());
     $this->assertSame('GitHub.com', (string)$response->getHeader('Server'));
     $this->assertEquals($github->getCredentials()->scopes, explode(', ', (string)$response->getHeader('X-OAuth-Scopes')));
+  }
+
+  public function testGistCreationAndDeletion() {
+    $api = $this->github()->api();
+
+    // Prepare gist post body
+    $gist = json_encode(array(
+      'description' => 'Test gist',
+      'public' => FALSE,
+      'files' => array(
+        'file1.txt' => array(
+          'content' => 'This is test gist content',
+        )
+      )
+    ));
+
+    // Create gist
+    $responseObject = $api->post('/gists', NULL, $gist)->send();
+    $this->assertSame(201, $responseObject->getStatusCode());
+
+    // Validate response
+    $response = $responseObject->json();
+    $this->assertInternalType('array', $response);
+    $this->assertArrayHasKey('id', $response);
+    $this->assertNotEmpty($response['id']);
+    $this->assertSame('Test gist', @$response['description']);
+
+    // Delete gist
+    $responseObject = $api->delete('/gists/'.rawurlencode($response['id']))->send();
+    $this->assertSame(204, $responseObject->getStatusCode());
+  }
+
+  private function github() {
+    $github = new Github();
+    $github->setServiceCredentials(Credentials::fromArray($this->keys('github', 'app')));
+    $github->setCredentials(Credentials::fromArray($this->keys('github', 'user')));
+    return $github;
   }
 }
