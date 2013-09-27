51eac743d5ae4c3881e2e12847401bd55adf8c7e
dangoodman <dan.ponomarev@gmail.com>
2013-09-27 18:25:39 +0400

    Disable service credentials validation for Google

--- lib/Drupal/wconsumer/Service/Google.php
+++ lib/Drupal/wconsumer/Service/Google.php
@@ -1,6 +1,7 @@
 <?php
 namespace Drupal\wconsumer\Service;
 
+use Drupal\wconsumer\Authentication\Credentials;
 use Drupal\wconsumer\Authentication\Oauth2\Oauth2;
 
 
@@ -20,6 +21,12 @@ class Google extends Base {
     $meta->registerAppUrl       = 'https://cloud.google.com/console';
 
     return $meta;
+  }
+
+  public function validateServiceCredentials(Credentials $credentials) {
+    // Disable service credentials validation since Google always returns 400 Bad Request error
+    // and it's not possible to detect if that's due to invalid credentials, invalid code or malformed request.
+    return TRUE;
   }
 
   protected function initAuthentication() {
