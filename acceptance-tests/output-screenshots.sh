a08e5aceb19d1c8a22bd8ad403d677a602a4c2b7
dangoodman <dan.ponomarev@gmail.com>
2013-10-09 16:27:28 +0400

    Fix output-screenshots.sh to work correctly with sh (as opposed to bash)

--- acceptance-tests/output-screenshots.sh
+++ acceptance-tests/output-screenshots.sh
@@ -5,7 +5,7 @@ set -e
 screenshots_dir="./build/screenshots"
 screenshots=$(find $screenshots_dir -name "*.png" -type f)
 
-if [ -n $screenshots ] ; then
+if [ ! -z $screenshots ] ; then
   echo "Below are screenshots of failed tests in base64 encoding:";
   for f in $screenshots; do
     echo "\n\n\n";
