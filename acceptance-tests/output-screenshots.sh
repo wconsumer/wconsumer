0cdf54d810007b18324f02b5343eafc8e402e66e
dangoodman <dan.ponomarev@gmail.com>
2013-10-09 16:56:35 +0400

    fix output-screenshots.sh

--- acceptance-tests/output-screenshots.sh
+++ acceptance-tests/output-screenshots.sh
@@ -5,7 +5,7 @@ set -e
 screenshots_dir="./build/screenshots"
 screenshots=$(find $screenshots_dir -name "*.png" -type f)
 
-if [ ! -z $screenshots ] ; then
+if [ ! -z "$screenshots" ] ; then
   echo "Below are screenshots of failed tests in base64 encoding:";
   for f in $screenshots; do
     echo -e "\n\n\n";
