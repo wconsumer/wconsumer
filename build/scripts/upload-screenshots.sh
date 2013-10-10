7c93cd5bbde3bf7e87a558801193efb639eaebc5
dangoodman <dan.ponomarev@gmail.com>
2013-10-10 16:12:46 +0400

    testing image upload without ./jq

--- build/scripts/upload-screenshots.sh
+++ build/scripts/upload-screenshots.sh
@@ -6,13 +6,13 @@ screenshots_dir="./build/screenshots"
 screenshots=$(find $screenshots_dir -name "*.png" -type f)
 
 if [ ! -z "$screenshots" ] ; then
-  wget --quiet "http://stedolan.github.io/jq/download/linux32/jq"
-  chmod +x ./jq
+  #wget --quiet "http://stedolan.github.io/jq/download/linux32/jq"
+  #chmod +x ./jq
   echo "Failed tests screenshots";
   for screenshot in $screenshots; do
     echo -en "${screenshot} "
     response=`curl -sS --header "Authorization: Client-Id 18fdc66f59434c9" -F "image=@$screenshot" https://api.imgur.com/3/image`
-    link=`echo "$response" | ./jq '.data.link'`
+    #link=`echo "$response" | ./jq '.data.link'`
     if [ -z "$link" ] || [ "$link" = "null" ] ; then
       echo "$response"
     else
