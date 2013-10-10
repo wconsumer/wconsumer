95631f2fbc56945a45042433b0f0406f82cb62b2
dangoodman <dan.ponomarev@gmail.com>
2013-10-10 21:37:54 +0400

    Rewrite upload-screenshots.sh

--- build/scripts/upload-screenshots.sh
+++ build/scripts/upload-screenshots.sh
@@ -7,14 +7,10 @@ screenshots=$(find $screenshots_dir -name "*.png" -type f)
 
 if [ ! -z "$screenshots" ] ; then
   echo "Failed tests screenshots";
+  wget http://imgur.com/tools/imgurbash.sh -O ./imgurbash.sh && chmod +x ./imgurbash.sh
+
   for screenshot in $screenshots; do
     echo -en "${screenshot} "
-    response=`curl -sS --header "Authorization: Client-Id 18fdc66f59434c9" -F "image=@$screenshot" https://api.imgur.com/3/image`
-    link=`echo '$response' | grep '"http:[^"]\+"' | tr -d '"' | sed 's/\\\//\//g'`
-    if [ -z "$link" ] || [ "$link" = "null" ] ; then
-      echo "$response"
-    else
-      echo "$link"
-    fi
+    ./imgurbash.sh '$screenshot' 2>/dev/null
   done
 fi
